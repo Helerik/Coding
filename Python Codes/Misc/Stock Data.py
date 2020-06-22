7baa6dbec0d3bfa6049c62f62c42518afe0e4f33
Helerik <60837069+Helerik@users.noreply.github.com>
2020-06-22 14:24:32 -0300

    plotGraphics now accepts different plot styles
    Can plot with lines or barplot. I'm planing on doing candlestick next.

--- Python Codes/Misc/Stock Data.py
+++ Python Codes/Misc/Stock Data.py
@@ -15,14 +15,26 @@ import pandas_datareader.data as web
 # Creates moving average of primaryName, if it isn't in secondaryPlot
 def movingAverage(dataFrame, primaryName, timePeriod = 15):
 
+    # Separate primaryName from primaryMode
+    primaryName = primaryName.split(', ', 1)[0]
+
     # Create new data frame with new column
     newFrame = dataFrame.copy(deep = True)
     newFrame['Moving Average ' + primaryName] = newFrame[primaryName].rolling(window = timePeriod, min_periods = 0).mean()
+    
     return newFrame
 
-def plotGraphics(dataFrame, primaryName, subplot = [], figsize = (14.4/1.5, 9.6/1.5)):
+def plotGeneral(dataFrame, primaryName, subplot = [], figsize = (14.4/1.5, 9.6/1.5)):
     # Creates a figure
     plt.figure(num = primaryName, figsize = figsize)
+
+    # Separate primaryName from primaryMode
+    try:
+        NameMode = primaryName.split(', ', 1)
+        primaryName = NameMode[0]
+        primaryMode = NameMode[1].lower()
+    except:
+        primaryMode = 'std'
     
     secondaryPlot = [
         'Volume'
@@ -30,10 +42,14 @@ def plotGraphics(dataFrame, primaryName, subplot = [], figsize = (14.4/1.5, 9.6/
 
     # Finds if a second plot window will be needed
     secPlot_key = 0
-    for plotName in subplot:
+    for i in range(len(subplot)):
+        subplot[i] = subplot[i].split(', ', 1)
+        if len(subplot[i]) == 1:
+            subplot[i] = [subplot[i][0], 'std']
+        plotName = subplot[i][0]
+            
         if plotName in secondaryPlot:
-            secPlot_key = 1
-            break
+            secPlot_key = 1 
 
     # If there is any subplot and one more window will be needed:
     if len(subplot) > 0 and secPlot_key:
@@ -41,29 +57,60 @@ def plotGraphics(dataFrame, primaryName, subplot = [], figsize = (14.4/1.5, 9.6/
 
         # Prepares primaryName plot
         ax1 = plt.subplot2grid((7,1), (0,0), rowspan = 5, colspan = 1)
-        ax1.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+
+        # Plot primaryName with the mode
+        if primaryMode == 'std':
+            ax1.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        elif primaryMode == 'bar':
+            ax1.bar(dataFrame.index, dataFrame[primaryName], label = primaryName)
+            
         for plotName in subplot:
             # If the secondary plot needs a second window, creates a second window +
             # plots the graphic. Can only have one more window; greedy algorithm (for now).
-            if plotName in secondaryPlot and not secPlot_key:
+            if plotName[0] in secondaryPlot and not secPlot_key:
                 ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 2, colspan = 1)
-                ax2.plot(dataFrame.index, dataFrame[plotName], label = plotName)
+
+                if plotName[1].lower() == 'std':
+                    ax2.plot(dataFrame.index, dataFrame[plotName[0]], label = plotName[0])
+                elif plotName[1].lower() == 'bar':
+                    ax2.bar(dataFrame.index, dataFrame[plotName[0]], label = plotName[0])
+                
                 ax2.legend()
                 secPlot_key = 1
             # Plots all secondary plots that don't need a second window on main window.
-            elif not plotName in secondaryPlot:
-                ax1.plot(dataFrame.index, dataFrame[plotName], label = plotName)
+            elif not plotName[0] in secondaryPlot:
+                
+                if plotName[1].lower() == 'std':
+                    ax1.plot(dataFrame.index, dataFrame[plotName[0]], label = plotName[0])
+                if plotName[1].lower() == 'bar':
+                    ax1.bar(dataFrame.index, dataFrame[plotName[0]], label = plotName[0])
+                    
         ax1.legend()
 
     # If more windows are not needed but there are secondary plots:
     elif len(subplot) > 0 and not secPlot_key:
-        plt.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        
+        if primaryMode == 'std':
+            plt.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        elif primaryMode == 'bar':
+            plt.bar(dataFrame.index, dataFrame[primaryName], label = primaryName)
+            
         for plotName in subplot:
-            plt.plot(dataFrame.index, dataFrame[plotName], label = plotName)
+            
+            if plotName[1].lower() == 'std':
+                ax1.plot(dataFrame.index, dataFrame[plotName[0]], label = plotName[0])
+            if plotName[1].lower() == 'bar':
+                ax1.bar(dataFrame.index, dataFrame[plotName[0]], label = plotName[0])
+                
         plt.legend()
     # Else, plots primaryName only, even if it is in secondaryPlot
     else:
-        plt.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        
+        if primaryMode == 'std':
+            plt.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        elif primaryMode == 'bar':
+            plt.bar(dataFrame.index, dataFrame[primaryName], label = primaryName)
+            
         plt.legend()
 
     plt.show()
@@ -92,6 +139,12 @@ def main():
     df = movingAverage(df, 'Adj Close', days)
     
     # Plots graphic
-    plotGraphics(df, 'Adj Close', ['Moving Average Adj Close', 'High'])
+    plotGeneral(df, 'Adj Close, Std', ['Moving Average Adj Close', 'High', 'Volume, Bar'])
 
 main()
+
+
+
+
+
+
