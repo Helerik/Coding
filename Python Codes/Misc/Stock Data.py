95f757a9c8cdb1b2f178eaf74544ccc208c9a3c5
Helerik <60837069+Helerik@users.noreply.github.com>
2020-06-22 12:49:53 -0300

    Big Update: new general plotting func + comments
    General plotting function so that plotting is better organized/standardized. Also added comments to the entire code, so that it will be more clear.

--- Python Codes/Misc/Stock Data.py
+++ Python Codes/Misc/Stock Data.py
@@ -1,43 +1,97 @@
+#!/usr/bin/env python3
+
 import warnings
 
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+from mplfinance.original_flavor import candlestick_ohlc
+import matplotlib.dates as mdates
 import pandas as pd
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas_datareader.data as web
 
-def movingAverage(dataFrame, timePeriod):
-    dataFrame['Moving Average'] = dataFrame['Adj Close'].rolling(window = timePeriod, min_periods = 0).mean()
-    return
+# Creates moving average of primaryName, if it isn't in secondaryPlot
+def movingAverage(dataFrame, primaryName, timePeriod = 15):
+
+    # Create new data frame with new column
+    newFrame = dataFrame.copy(deep = True)
+    newFrame['Moving Average ' + primaryName] = newFrame[primaryName].rolling(window = timePeriod, min_periods = 0).mean()
+    return newFrame
+
+def plotGraphics(dataFrame, primaryName, subplot = [], figsize = (14.4/1.5, 9.6/1.5)):
+    # Creates a figure
+    plt.figure(num = primaryName, figsize = figsize)
     
+    secondaryPlot = [
+        'Volume'
+        ]
+
+    # Finds if a second plot window will be needed
+    secPlot_key = 0
+    for plotName in subplot:
+        if plotName in secondaryPlot:
+            secPlot_key = 1
+            break
+
+    # If there is any subplot and one more window will be needed:
+    if len(subplot) > 0 and secPlot_key:
+        secPlot_key = 0
+
+        # Prepares primaryName plot
+        ax1 = plt.subplot2grid((7,1), (0,0), rowspan = 5, colspan = 1)
+        ax1.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        for plotName in subplot:
+            # If the secondary plot needs a second window, creates a second window +
+            # plots the graphic. Can only have one more window; greedy algorithm (for now).
+            if plotName in secondaryPlot and not secPlot_key:
+                ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 2, colspan = 1)
+                ax2.plot(dataFrame.index, dataFrame[plotName], label = plotName)
+                ax2.legend()
+                secPlot_key = 1
+            # Plots all secondary plots that don't need a second window on main window.
+            elif not plotName in secondaryPlot:
+                ax1.plot(dataFrame.index, dataFrame[plotName], label = plotName)
+        ax1.legend()
+
+    # If more windows are not needed but there are secondary plots:
+    elif len(subplot) > 0 and not secPlot_key:
+        plt.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        for plotName in subplot:
+            plt.plot(dataFrame.index, dataFrame[plotName], label = plotName)
+        plt.legend()
+    # Else, plots primaryName only, even if it is in secondaryPlot
+    else:
+        plt.plot(dataFrame.index, dataFrame[primaryName], label = primaryName)
+        plt.legend()
+
+    plt.show()
+    return True
 
 def main():
-    
+
+    # Styling
     style.use('ggplot')
 
+    # Period 
     start = dt.datetime(2020,1,1)
     end = dt.date.today()
 
+    # Stock data
     df = web.DataReader('^BVSP', 'yahoo', start, end)
 
+    # Header
     print("Today:", end)
     print(df.head())
     print()
+    input('>>')
 
-    days = 10
-
-    movingAverage(df, days)
-
-    ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan = 1)
-    ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 1, colspan = 1)
-
-    ax1.plot(df.index, df['Adj Close'], label = "Adj Close")
-    ax1.plot(df.index, df['Moving Average'], label = "Moving average for " + str(days) + " days")
-    ax1.legend()
-    ax2.bar(df.index, df['Volume'], label = "Volume")
-    ax2.legend()
-    plt.show()
+    # Creates moving average
+    days = 15
+    df = movingAverage(df, 'Adj Close', days)
+    
+    # Plots graphic
+    plotGraphics(df, 'Adj Close', ['Moving Average Adj Close', 'High'])
 
 main()
