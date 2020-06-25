4c8b17b3b391cd2e40dd1aa7733725f2f833edb6
Helerik <60837069+Helerik@users.noreply.github.com>
2020-06-25 06:08:10 -0300

    Added example run

--- Cursos/Coursera Deep Learning Specialization - Ongoing/1 - Neural Network and Deep Learning/Week 2/Part2.py
+++ Cursos/Coursera Deep Learning Specialization - Ongoing/1 - Neural Network and Deep Learning/Week 2/Part2.py
@@ -5,40 +5,83 @@
 import numpy as np
 
 class LogisticRegressor():
-
     def __init__(self, trainX, trainY):
+
+        # Training data
         self.X = trainX
         self.y = trainY
-        self.m, self.n = trainX.shape
+
+        # Training data size
+        self.n, self.m = trainX.shape
         if self.m != len(trainY):
-            raise ValueError("Invalid vector sizes for trainX and trainY -> trainX size = " + self.m + " while trainY size = " + len(trainY))
-        self.w = np.random.uniform(-1,1,n)
+            raise ValueError("Invalid vector sizes for trainX and trainY -> trainX size = " + str(self.m) + " while trainY size = " + str(len(trainY)))
+
+        # Parameter vector
+        self.w = np.random.uniform(-1,1,self.n)
         self.b = 0
 
+        self.isTrained = 0
+
     # Sigmoid activation function
-    def sigmoid(t):
+    def sigmoid(self, t):
+        warnings.filterwarnings("")
         return 1/(1+np.exp(-t))
 
     # Loss function for  logistic regression
-    def lossFunc(y, y_pred):
+    def lossFunc(self, y, y_pred):
         return -(y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
 
     # Gradient descent for logistic regression
-    def gradientDescent(self, b, alpha, max_iter):
+    def gradientDescent(self, alpha, max_iter):
+
+        X = self.X
+        y = self.y
+        w = self.w
+        b = self.b
+        m = self.m
         
         for _ in range(max_iter):
             
-            Z = np.dot(self.w.T, self.X) + b
-            y_pred = sigmoid(Z)
-            costFunc = np.sum(lossFunc(self.y, y_pred))/self.m
+            Z = np.dot(w.T, X) + b
+            y_pred = self.sigmoid(Z)
+            costFunc = np.sum(self.lossFunc(self.y, y_pred))/m
 
             dz = y_pred - self.y
-            dw = np.dot(self.X, dz.T)/self.m
-            db = np.sum(dz)/self.m
+            dw = np.dot(self.X, dz.T)/m
+            db = np.sum(dz)/m
 
-            self.w -= alpha*dw
+            w -= alpha*dw
             b -= alpha*db
 
+        self.X = X
+        self.y = y
+        self.w = w
         self.b = b
 
-        return [w, b]
+        self.isTrained = 1
+
+def example():
+    
+    X = np.array([
+        [780,750,690,710,680,730,690,720,740,690,610,690,710,680,770,610,580,650,540,590,620,600,550,550,570,670,660,580,650,660,640,620,660,660,680,650,670,580,590,690],
+        [4,3.9,3.3,3.7,3.9,3.7,2.3,3.3,3.3,1.7,2.7,3.7,3.7,3.3,3.3,3,2.7,3.7,2.7,2.3,3.3,2,2.3,2.7,3,3.3,3.7,2.3,3.7,3.3,3,2.7,4,3.3,3.3,2.3,2.7,3.3,1.7,3.7],
+        [3,4,3,5,4,6,1,4,5,1,3,5,6,4,3,1,4,6,2,3,2,1,4,1,2,6,4,2,6,5,1,2,4,6,5,1,2,1,4,5],
+        ])
+    y = np.array([1,1,0,1,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,0,1,0,0,0,0,1,1,0,1,1,0,0,1,1,1,0,0,0,0,1])
+
+    LR = LogisticRegressor(X, y)
+
+    LR.gradientDescent(0.01, 1000)
+    print(LR.isTrained)
+
+
+
+example()
+
+
+
+
+
+
+
+    
