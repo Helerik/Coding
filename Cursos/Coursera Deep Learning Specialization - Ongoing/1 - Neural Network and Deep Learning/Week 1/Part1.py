04d80761f276cdbeeeed4db7007f0fc209174885
Helerik <60837069+Helerik@users.noreply.github.com>
2020-06-25 03:35:59 -0300

    New class for logistic regression created

--- Cursos/Coursera Deep Learning Specialization - Ongoing/1 - Neural Network and Deep Learning/Week 1/Part1.py
+++ Cursos/Coursera Deep Learning Specialization - Ongoing/1 - Neural Network and Deep Learning/Week 1/Part1.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
 
 import numpy as np
 
-def sigmoid(t):
-    return 1/(1+np.exp(-t))
+class LogisticLearner():
 
-def logisticRegression(X, w, b):
-    return sigmoid(np.dot(w.T, x) + b)
+    # Constructor
+    def __init__(self, trainX, trainY):
+        self.trainX = trainX
+        self.trainY = trainY
+
+    def sigmoid(t):
+        return 1/(1+np.exp(-t))
+
+    def logisticRegression(x, w, b):
+        return sigmoid(np.dot(w.T, x) + b)
+
+    def lossFunc(y, y_t):
+        return -(y*np.log(y_t) + (1-y)*log(1-y_t))
     
