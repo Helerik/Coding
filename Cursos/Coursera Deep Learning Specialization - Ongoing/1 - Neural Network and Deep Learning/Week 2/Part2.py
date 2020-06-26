805cce2018ad5df70003429a2fd580f68ec1754d
Helerik <60837069+Helerik@users.noreply.github.com>
2020-06-26 12:34:03 -0300

    Plots cost function

--- Cursos/Coursera Deep Learning Specialization - Ongoing/1 - Neural Network and Deep Learning/Week 2/Part2.py
+++ Cursos/Coursera Deep Learning Specialization - Ongoing/1 - Neural Network and Deep Learning/Week 2/Part2.py
@@ -3,6 +3,7 @@
 # Vectorized implementation of logistic regression
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def model(trainX, trainY, alpha, max_iter):
 
@@ -24,11 +25,13 @@ def sigmoid(t):
 # Gradient descent for logistic regression
 def gradientDescent(X, y, w, b, alpha, max_iter):
 
+    cost = []
     m = X.shape[1]
     for _ in range(max_iter):
 
         y_pred = sigmoid(np.dot(w.T, X) + b)
         costFunc = np.sum(-(y*np.log(y_pred) + (1-y)*np.log(1-y_pred)))/m
+        cost.append(costFunc)
 
         dz = y_pred - y
         dw = np.dot(X, dz.T)/m
@@ -36,6 +39,10 @@ def gradientDescent(X, y, w, b, alpha, max_iter):
 
         w = w - alpha*dw
         b = b - alpha*db
+
+    plt.plot(cost)
+    plt.title("Gradient Descent")
+    plt.show(block = False)
 
     return [w, b]
 
@@ -58,8 +65,9 @@ def example():
         [3,4,3,5,4,6,1,4,5,1,3,5,6,4,3,1,4,6,2,3,2,1,4,1,2,6,4,2,6,5,1,2,4,6,5,1,2,1,4,5],
         ])
     y = np.array([[1,1,0,1,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,0,1,0,0,0,0,1,1,0,1,1,0,0,1,1,1,0,0,0,0,1]])
+    X[0] = X[0]/100
 
-    w, b = model(X, y, 0.00001, 100000)
+    w, b = model(X, y, 0.05, 100000)
 
     pred = predict(X, w, b)
 
