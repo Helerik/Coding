ad8fe589c259210a5fb33c14f33ab76176e9c7d8
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-03 11:56:14 -0300

    Changed Example

--- Python Codes/Newton Method Neural Network/Newton Method NN.py
+++ Python Codes/Newton Method Neural Network/Newton Method NN.py
@@ -228,7 +228,6 @@ def example():
     y_train = np.array([y_train])
     y_test = np.array([y_test])
 
-    layers = [5, 5, 5]
     layers = [10, 10, 10]
     weights = model(X_train, y_train, layers, 0.1, max_iter = 500, plot_N = 10)
     
