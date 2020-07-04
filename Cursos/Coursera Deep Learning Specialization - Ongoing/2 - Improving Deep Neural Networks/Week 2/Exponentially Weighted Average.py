466984aa1e8584aa28f5871146d60560b82ec965
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-04 11:12:53 -0300

    Added bias correction + changed t for i

--- Cursos/Coursera Deep Learning Specialization - Ongoing/2 - Improving Deep Neural Networks/Week 2/Exponentially Weighted Average.py
+++ Cursos/Coursera Deep Learning Specialization - Ongoing/2 - Improving Deep Neural Networks/Week 2/Exponentially Weighted Average.py
@@ -8,20 +8,24 @@ import matplotlib.pyplot as plt
 def EWA(data, beta):
 
     avg = []
-    v_t_prev = 0
+    v_i_prev = 0
     
     for i in range(len(data)):
+
+        bias_correction = 1-beta**(i+1)
         
-        v_t = beta*v_t_prev + (1-beta)*data[i]
-        avg.append(v_t)
-        v_t_prev = v_t
+        v_i = beta*v_i_prev + (1-beta)*data[i]
+        v_i_prev = v_i
+        v_i /= bias_correction
+        avg.append(v_i)
+        
 
     return avg
 
 def example():
-    X = [0,1,4,5,6,7,6,5,3,4,5,6,1,2,3,1,0,0,3,2,1,4,5,6,7,8,9,9,9,7,8,9,7,6,5,4,5,3,2,2]
+    X = [9,7,6,4,6,7,6,5,3,4,5,6,1,2,3,1,0,0,3,2,1,4,5,6,7,8,9,9,9,7,8,9,7,6,5,4,5,3,2,2]
     y = range(len(X))
-    avg = EWA(X, 0.5)
+    avg = EWA(X, 0.6)
 
     plt.scatter(y, X, color = 'r')
     plt.plot(y, avg)
