63cfe6b31a19e75189569a4908da7d3fa4dcd603
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-03 23:26:15 -0300

    Changed NN implementation to a class implementation

--- Python Codes/Multilayered Neural Network/Neural Network.py
+++ Python Codes/Multilayered Neural Network/Neural Network.py
@@ -4,177 +4,281 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Fits for X and Y
-def model(X, Y, layer_sizes, learning_rate, L2 = 0, max_iter = 100, plot_N = 100):
+class NeuralNetwork():
 
-    n_x, m_x = X.shape
-    n_y, m_y = Y.shape
-    if m_x != m_y:
-        raise ValueError(f"Invalid vector sizes for X and Y -> X size = {X.shape} while Y size = {Y.shape}.")
+    # Initializes Neural Network structure
+    def __init__(self, layer_sizes = [1], learning_rate = 0.01, L2 = 0, max_iter = 200, activation = 'sigmoid', plot_N = None):
 
-    weights = initialize_weights(n_x, n_y, layer_sizes)
-    num_layers = len(layer_sizes)
+        # Structural variables
+        self.layer_sizes = layer_sizes
+        self.num_layers = len(layer_sizes) + 1
+        self.learning_rate = learning_rate
+        self.L2 = L2
+        self.max_iter = max_iter
+        self.plot_N = plot_N
 
-    return gradient_descent(X, Y, weights, learning_rate, L2, num_layers, max_iter, plot_N)
+        # Activation function
+        if isinstance(activation, str):
+            if activation.lower() == 'sigmoid':
+                self.activation = Sigmoid
+            elif activation.lower() == 'tanh':
+                self.activation = Tanh
+            elif activation.lower() == 'ltanh' \
+            or activation.lower() == 'l-tanh' \
+            or activation.lower() == 'leakytanh' \
+            or activation.lower() == 'leaky tanh' \
+            or activation.lower() == 'leaky-tanh':
+                self.activation = LeakyTanh
+            elif activation.lower() == 'relu':
+                self.activation = ReLu
 
-# Sigmoid activation function
-def sigmoid(t):
-    return 1/(1+np.exp(-t))
-
-# Initializes weights for each layer
-def initialize_weights(n_x, n_y, layer_sizes, seed = None):
-    
-    np.random.seed(seed)
-    
-    weights = {}
-    n_h_prev = n_x
-    for i in range(len(layer_sizes)):
-        n_h = layer_sizes[i]
-        weights['W'+str(i+1)] = np.random.randn(n_h, n_h_prev)*np.sqrt(1/n_h_prev)
-        weights['b'+str(i+1)] = np.zeros((n_h,1))
-        n_h_prev = n_h
-    
-    weights['W'+str(len(layer_sizes)+1)] = np.random.random((n_y, n_h_prev))*np.sqrt(1/n_h_prev)
-    weights['b'+str(len(layer_sizes)+1)] = np.zeros((n_y,1))
-
-    np.random.seed(None)
-    
-    return weights
-
-# Performs foward propagation
-def forward_propagation(weights, X, num_layers):
-
-    # Cache for A
-    A_vals = {}
-    Ai_prev = np.copy(X)
-    for i in range(num_layers+1):
+        # Variable variables
+        self.X = None
+        self.Y = None
+        self.m = None
         
-        Wi = weights['W'+str(i+1)]
-        bi = weights['b'+str(i+1)]
+        self.weights = {}
+        self.best_weights = {}
+        self.A_vals = {}
+        self.Z_vals = {}
+
+    # Initializes weights for each layer
+    def __initialize_weights(self, n_x, n_y):
+        
+        n_h_prev = n_x
+        for i in range(self.num_layers - 1):
+            n_h = self.layer_sizes[i]
+            self.weights['W'+str(i+1)] = np.random.randn(n_h, n_h_prev)*np.sqrt(1/n_h_prev)
+            self.weights['b'+str(i+1)] = np.zeros((n_h,1))
+            n_h_prev = n_h
+        
+        self.weights['W'+str(self.num_layers)] = np.random.random((n_y, n_h_prev))*np.sqrt(1/n_h_prev)
+        self.weights['b'+str(self.num_layers)] = np.zeros((n_y,1))
+    
+
+    # Fits for X and Y
+    def fit(self, X, Y):
+        
+        n_x, m_x = X.shape
+        n_y, m_y = Y.shape
+        if m_x != m_y:
+            raise ValueError(f"Invalid vector sizes for X and Y -> X size = {X.shape} while Y size = {Y.shape}.")
+
+        self.__initialize_weights(n_x, n_y)
+
+        self.X = X
+        self.Y = Y
+        self.m = m_x
+
+        self.__gradient_descent()
+
+    # Performs foward propagation
+    def __forward_propagation(self):
+
+        Ai_prev = np.copy(self.X)
+        for i in range(self.num_layers - 1):
+            
+            Wi = self.weights['W'+str(i+1)]
+            bi = self.weights['b'+str(i+1)]
+            
+            Zi = np.dot(Wi, Ai_prev) + bi
+            Ai = self.activation.function(Zi)
+
+            self.A_vals['A'+str(i+1)] = Ai
+            self.Z_vals['Z'+str(i+1)] = Zi
+            
+            Ai_prev = np.copy(Ai)
+
+        # Last layer always receives sigmoid
+        Wi = self.weights['W'+str(self.num_layers)]
+        bi = self.weights['b'+str(self.num_layers)]
         
         Zi = np.dot(Wi, Ai_prev) + bi
-        Ai = sigmoid(Zi)
+        Ai = Sigmoid.function(Zi)
 
-        A_vals['A'+str(i+1)] = Ai
-        Ai_prev = np.copy(Ai)
+        self.A_vals['A'+str(self.num_layers)] = Ai
+        self.Z_vals['Z'+str(self.num_layers)] = Zi
 
-    return A_vals
-    
-
-# Newton method for training a neural network
-def gradient_descent(X, Y, weights, learning_rate, L2, num_layers, max_iter, plot_N):
-
-    # Initialize constant m
-    m = X.shape[1]
-    
-    # Cache for ploting cost
-    cost = []
-    iteration = []
-
-    # Cache for minimum cost and best weights
-    best_weights = weights.copy()
-    min_cost = np.inf
-
-    # Init break_code = 0
-    break_code = 0
-    for it in range(max_iter):
-
-        # Forward propagation
-        A_vals = forward_propagation(weights, X, num_layers)
-
-        # Evaluates cost fucntion
-        AL = np.copy(A_vals['A'+str(num_layers+1)])
-        loss_func = -(Y*np.log(AL) + (1-Y)*np.log(1-AL))
+    # Evaluates cost function
+    def __evaluate_cost(self):
+        
+        AL = np.copy(self.A_vals['A'+str(self.num_layers)])
+        loss_func = -(self.Y*np.log(AL) + (1-self.Y)*np.log(1-AL))
         cost_func = np.mean(loss_func)
 
-        # Evaluates regularization
-        if L2 > 0:
+        # Evaluates regularization cost
+        if self.L2 > 0:
             L2_reg = 0
-            for i in range(1, num_layers+1):
-                L2_reg += np.sum(np.square(weights['W'+str(i)]))
-            L2_reg *= L2/(2*m)
+            for i in range(1, self.num_layers):
+                L2_reg += np.sum(np.square(self.weights['W'+str(i)]))
+            L2_reg *= self.L2/(2*self.m)
             cost_func += L2_reg
+
+        return cost_func
+
+    # Newton method for training a neural network
+    def __gradient_descent(self):
+
+        # Initialize constant m
+        m = self.m
         
-        # Updates best weights
-        if cost_func < min_cost:
-            best_weights = weights.copy()
-            min_cost = cost_func
+        # Cache for ploting cost
+        cost = []
+        iteration = []
+
+        # Cache for minimum cost and best weights
+        self.best_weights = self.weights.copy()
+        min_cost = np.inf
+
+        # Init break_code = 0
+        break_code = 0
+        for it in range(self.max_iter):
+
+            # Forward propagation
+            self.__forward_propagation()
+
+            # Evaluate cost
+            cost_func = self.__evaluate_cost()
             
-        # Caches cost function every plot_N iterations
-        if it%plot_N == 0:
+            # Updates best weights
+            if cost_func < min_cost:
+                self.best_weights = self.weights.copy()
+                min_cost = cost_func
+                
+            # Caches cost function every plot_N iterations
+            if self.plot_N != None:
+                if (it + 1) % self.plot_N == 0:
+                    cost.append(cost_func)
+                    iteration.append(it+1)
+                    
+                    plt.clf()
+                    plt.plot(iteration, cost, color = 'b')
+                    plt.xlabel("Iteration")
+                    plt.ylabel("Cost Function")
+                    plt.title(f"Cost Function after {iteration[-1]} iterations:")
+                    plt.pause(0.001)
+
+
+            # Backward propagation
+            for i in range(self.num_layers, 0, -1):
+
+                # Gets current layer weights
+                Wi = np.copy(self.weights['W'+str(i)])
+                bi = np.copy(self.weights['b'+str(i)])
+                Ai = np.copy(self.A_vals['A'+str(i)])
+                Zi = np.copy(self.Z_vals['Z'+str(i)])
+
+                # If on first layer, Ai_prev = X itself
+                if i == 1:
+                    Ai_prev = np.copy(self.X)
+                else:
+                    Ai_prev = np.copy(self.A_vals['A'+str(i-1)])
+
+                # If on the last layer, dZi = Ai - Y; else dZi = (Wi+1 . dZi+1) * (Ai*(1-Ai))
+                if i == self.num_layers:
+                    dZi = Ai - self.Y
+                else:
+                    dZi = np.dot(Wnxt.T, dZnxt) * self.activation.derivative(Zi)
+
+                # Calculates dWi and dbi
+                dWi = np.dot(Ai_prev, dZi.T)/m + (self.L2/m)*Wi.T
+                dbi = np.sum(dZi, axis = 1, keepdims = 1)/m
+
+                # Cache dZi, Wi
+                dZnxt = np.copy(dZi)
+                Wnxt = np.copy(Wi)     
+
+                # Updates weights and biases
+                Wi = Wi - self.learning_rate*dWi.T
+                bi = bi - self.learning_rate*dbi
+                
+                self.weights['W'+str(i)] = Wi
+                self.weights['b'+str(i)] = bi
+                
+            # End of backprop loop ==================================================
+
+        if self.plot_N != None:
+            
+            # Final cost evaluation:
+            cost_func = self.__evaluate_cost()
             cost.append(cost_func)
-            iteration.append(it)
-
-        # Backward propagation
-        for i in range(num_layers+1, 0, -1):
-
-            # Gets current layer weights
-            Wi = np.copy(weights['W'+str(i)])
-            bi = np.copy(weights['b'+str(i)])
-            Ai = np.copy(A_vals['A'+str(i)])
-
-            if i == 1:
-                A_prev = np.copy(X)
-            else:
-                A_prev = np.copy(A_vals['A'+str(i-1)])
-
-            # If on the last layer, dZi = Ai - Y; else dZi = (Wi+1 . dZi+1) * (Ai*(1-Ai))
-            if i == num_layers+1:
-                dZi = Ai - Y
-            else:
-                dZi = np.dot(Wnxt.T, dZnxt) * Ai * (1 - Ai)
-
-            # Calculates dWi and dbi
-            dWi = np.dot(A_prev, dZi.T)/m + (L2/m)*Wi.T
-            dbi = np.sum(dZi, axis = 1, keepdims = 1)/m
-
-            # Cache dZi, Wi
-            dZnxt = np.copy(dZi)
-            Wnxt = np.copy(Wi)     
-
-            # Updates weights and biases
-            Wi = Wi - learning_rate*dWi.T
-            bi = bi - learning_rate*dbi
-            weights['W'+str(i)] = Wi
-            weights['b'+str(i)] = bi
-
-        # Plot cost every plot_N iterations
-        if it % plot_N == 0:
+            iteration.append(it+1)
+            
+            # Plots cost function over time
             plt.clf()
             plt.plot(iteration, cost, color = 'b')
             plt.xlabel("Iteration")
             plt.ylabel("Cost Function")
-            plt.title("Gradient Descent for Cost Function")
-            plt.pause(0.001)
+            plt.title(f"Cost Function after {iteration[-1]} iterations:")
+            plt.show(block = 0)
+
+        self.X = None
+        self.Y = None
+        self.m = None
+
+        self.weights = {}
+        self.A_vals = {}
+
+    # Predicts if X vector tag is 1 or 0
+    def predict(self, X):
+
+        Ai_prev = np.copy(X)
+        for i in range(self.num_layers):
             
-        # End of backprop loop ==================================================
+            Wi = self.best_weights['W'+str(i+1)]
+            bi = self.best_weights['b'+str(i+1)]
+            
+            Zi = np.dot(Wi, Ai_prev) + bi
+            Ai = Ai = self.activation.function(Zi)
 
-        # Early breaking condition met
-        if break_code:
-            AL = A_vals['A'+str(num_layers+1)]
-            loss_func = -(Y*np.log(AL) + (1-Y)*np.log(1-AL))
-            cost_func = np.mean(loss_func)
-            cost.append(cost_func)
-            iteration.append(it)
-            break
+            self.A_vals['A'+str(i+1)] = Ai
+            Ai_prev = np.copy(Ai)
 
-    # Plots cost function over time
-    plt.clf()
-    plt.plot(iteration, cost, color = 'b')
-    plt.xlabel("Iteration")
-    plt.ylabel("Cost Function")
-    plt.title("Gradient Descent for Cost Function")
-    plt.show(block = 0)
+        return Ai > 0.5
 
-    return best_weights
+# Sigmoid class - contains sigmoid function and its derivative     
+class Sigmoid():
 
-# Predicts if X vector tag is 1 or 0
-def predict(weights, X, num_layers):
+    @classmethod
+    def function(cls, t):
+        return 1/(1+np.exp(-t))
 
-    A = forward_propagation(weights, X, num_layers)
-    A = A['A'+str(num_layers+1)]
+    @classmethod
+    def derivative(cls, t):
+        return cls.function(t)*(1-cls.function(t))
 
-    return A > 0.5
+# Tanh class - contains tanh function and its derivative     
+class Tanh():
+
+    @classmethod
+    def function(cls, t):
+        return np.tanh(t)
+
+    @classmethod
+    def derivative(cls, t):
+        return 1 - np.power(np.tanh(t), 2)
+
+# "Leaky" Tanh class - contains my idea for a "leaky" tanh function and its derivative
+class LeakyTanh():
+
+    @classmethod
+    def function(cls, t, leak = 0.2):
+        return np.tanh(t) + t*leak
+
+    @classmethod
+    def derivative(cls, t, leak = 0.2):
+        return 1 - np.power(np.tanh(t), 2) + leak
+
+# ReLu class = contains ReLu function and its derivative
+class ReLu():
+
+    @classmethod
+    def function(cls, t, leak = 0.0):
+        return np.maximum(t*leak, t)
+
+    @classmethod
+    def derivative(cls, t, leak = 0.0):
+        return np.where(t > 0, 1.0, leak)
 
 def example():
 
@@ -182,9 +286,9 @@ def example():
     from sklearn.preprocessing import StandardScaler as StdScaler
     from sklearn.model_selection import train_test_split
 
-    # Won't work without scalling data
+    # Won't work properly without scalling data
     scaler = StdScaler()
-    data = load_breast_cancer(return_X_y=True)
+    data = load_breast_cancer(return_X_y = True)
 
     X = data[0]
     scaler.fit(X)
@@ -198,10 +302,17 @@ def example():
     y_train = np.array([y_train])
     y_test = np.array([y_test])
 
-    layers = [10,10]
-    weights = model(X_train, y_train, layers, 0.1, L2 = 0, max_iter = 500, plot_N = 10)
+    clf = NeuralNetwork(
+        layer_sizes = [20,20,20,20],
+        learning_rate = 0.1,
+        L2 = 0,
+        max_iter = 500,
+        activation = 'relu',
+        plot_N = 20)
+
+    clf.fit(X_train, y_train)
     
-    pred = predict(weights, X_train, len(layers))
+    pred = clf.predict(X_train)
     percnt = 0
     for i in range(pred.shape[1]):
         if pred[0,i] == y_train[0,i]:
@@ -210,7 +321,7 @@ def example():
     print()
     print(f"Accuracy of {percnt:.2%} on training set")
 
-    pred = predict(weights, X_test, len(layers))
+    pred = clf.predict(X_test)
     percnt = 0
     for i in range(pred.shape[1]):
         if pred[0,i] == y_test[0,i]:
