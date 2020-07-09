a3771d1f21f247118b7a1a6a12896a5220b923ed
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-09 16:44:07 -0300

    Added new page

--- Python Codes/Flask/Flask Example.py
+++ Python Codes/Flask/Flask Example.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return "This is the homepage"
 
+@app.route('/some_name')
+def some_name():
+    return '<h2>This is a web page text in HTML</h2>'
+
 # Starts the web-server
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
