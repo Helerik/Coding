9e0682d24c93df12f180e34ad3cdf9aa4c837fd0
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-09 16:50:54 -0300

    Created function that passes str variable and int variable

--- Python Codes/Flask/Flask Example.py
+++ Python Codes/Flask/Flask Example.py
@@ -7,9 +7,17 @@ app = Flask(__name__)
 def index():
     return "This is the homepage"
 
-@app.route('/some_name')
+@app.route("/some_name")
 def some_name():
-    return '<h2>This is a web page text in HTML</h2>'
+    return "<h2>This is a web page text in HTML</h2>"
+
+@app.route("/profile/<username>") # <username> is a variable
+def profile(username):
+    return f"Hey there {username}."
+
+@app.route("/post/<int:post_id>") # if datatype != string, has to pass data type!
+def post(post_id):
+    return f"<h2>Post ID is {post_id}<h2>"
 
 # Starts the web-server
 if __name__ == "__main__":
