90976083758bcd2404a7f6d3c831ad00843ccd4d
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-09 16:41:13 -0300

    Corrected import error + made host = 0.0.0.0

--- Python Codes/Flask/Flask Example.py
+++ Python Codes/Flask/Flask Example.py
@@ -1,8 +1,12 @@
 
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/') #root directory, i.e. homepage
 def index():
     return "This is the homepage"
+
+# Starts the web-server
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True)
