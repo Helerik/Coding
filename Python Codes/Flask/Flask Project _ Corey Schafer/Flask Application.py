ba4b78a9e3af4873774bb96df3471413083ea2c3
Helerik <60837069+Helerik@users.noreply.github.com>
2020-07-10 14:59:43 -0300

    Update Flask Application.py

--- Python Codes/Flask/Flask Project _ Corey Schafer/Flask Application.py
+++ Python Codes/Flask/Flask Project _ Corey Schafer/Flask Application.py
@@ -1,12 +1,14 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World!"
+    return render_template("home_page.html")
 
+@app.route("/about")
+def about():
 
 
 
