83b4b867387485269ebdad60a831ede9252c91e3
Helerik <60837069+Helerik@users.noreply.github.com>
2020-08-17 17:43:21 -0300

    Created files for learning about javascript

---
+++ Cursos/HTML CSS and Javascript for Web Developers/Week 4/JS-Part1/js/script.js
@@ -0,0 +1,23 @@
+/* Defining variable - just like python, variables can be of any type! */
+var x = "Hello World!"
+
+/* Defining function */
+function func_name() {
+	pass;
+}
+
+/* or */
+var func_name = function () {
+	pass;
+}
+
+/* Example: */
+
+function compare(x, y) {
+	return x > y
+}
+
+var result = compare(4,5);
+console.log(result);
+
+/* ========================================================================= */
