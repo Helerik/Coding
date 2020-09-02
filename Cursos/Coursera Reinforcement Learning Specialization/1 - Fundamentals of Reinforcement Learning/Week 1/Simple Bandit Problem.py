72eed811e116adb1fd24ad813c75e52f2d95ef77
Helerik <60837069+Helerik@users.noreply.github.com>
2020-09-01 23:31:18 -0300

    Create Simple Bandit Problem.py

---
+++ Cursos/Coursera Reinforcement Learning Specialization/1 - Fundamentals of Reinforcement Learning/Week 1/Simple Bandit Problem.py
@@ -0,0 +1,40 @@
+# !/usr/bin/env python
+# Author: Erik Davino Vincent
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+
+    k = 4
+    eps = 0.1
+
+    Q = np.zeros(k)
+    N = np.zeros(k)
+    ground_truth = np.random.randn(k)
+
+    plot_cache = []
+
+    while True:
+
+        plot_cache.append(np.linalg.norm(Q - ground_truth))
+
+        plt.clf()
+        plt.plot(plot_cache)
+        plt.pause(0.01)
+        
+        Bandit = np.array([np.random.normal(ground_truth[i]) for i in range(k)])
+
+        if np.random.random() < 1 - eps:
+            A = np.argmax(Q)
+        else:
+            A = np.random.randint(0, k)
+            
+        R = Bandit[A]
+        N[A] = N[A] + 1
+        Q[A] = Q[A] + (1/N[A])*(R - Q[A])
+
+        
+
+main()
+        
