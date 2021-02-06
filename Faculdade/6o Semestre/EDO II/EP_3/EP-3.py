dd6d44fca6f29ae3e818b4cb1ca7599d26493f22
Helerik <60837069+Helerik@users.noreply.github.com>
2021-02-06 07:57:06 -0300

    Create EP-3.py

---
+++ Faculdade/6o Semestre/EDO II/EP_3/EP-3.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# !/usr/bin/env python3
+# Author: Erik Davino Vincent
+# NUSP: 10736584
+# MAP 2320 - Metodos Numericos em Equacoes Diferenciais II
+
+'''
+Problema:
+          
+
+'''
+
+import numpy as np
+from numpy import cos, sin, pi, exp
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import time
+
+
+
+
+
+
+
